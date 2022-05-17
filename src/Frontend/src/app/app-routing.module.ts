42161ea3d3ae4032ed1c1bb96c35cada6036d0e2
TheChec <chechoosorio@hotmail.com>
2022-05-17 07:17:46 -0500

    login

--- src/Frontend/src/app/app-routing.module.ts
+++ src/Frontend/src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import { HomeComponent} from  './home/home.component'
 import { SearchComponent} from './search/search.component'
 import { PreviewComponent} from './preview/preview.component'
 import { PartsComponent} from './parts/parts.component'
+import { InicioDeSesionComponent } from './inicio-de-sesion/inicio-de-sesion.component';
 
 const routes: Routes = [
+  { path: 'login', component: InicioDeSesionComponent },
   { path: 'home', component: HomeComponent },
   { path: 'search', component: SearchComponent },
   { path: 'preview', component: PreviewComponent },
   { path: 'parts', component: PartsComponent },]; // sets up routes constant where you define your routes
+
   
 
 // configures NgModule imports and exports
