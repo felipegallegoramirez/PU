c5b345aa19d4ebe15ac1f2271b0ce08c926d0cc8
TheChec <chechoosorio@hotmail.com>
2022-05-17 07:12:31 -0500

    login

--- src/Frontend/src/app/app-routing.module.ts
+++ src/Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioDeSesionComponent } from './inicio-de-sesion/inicio-de-sesion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ { path: "inicio-de-sesion", component: InicioDeSesionComponent }, 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/Frontend/src/app/app.module.ts
+++ src/Frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioDeSesionComponent } from './inicio-de-sesion/inicio-de-sesion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioDeSesionComponent
   ],
   imports: [
     BrowserModule,
